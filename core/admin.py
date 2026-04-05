2fee50d5b1802c85ac05234e61e1dda3d391b89c
Landry MMs <mmslandry8@gmail.com>
2026-04-05 16:16:31 +0100

    Étape 3 : Cartographie électorale (Commune, Centre, BV)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Commune, CentreVote, BureauVote
 # Register your models here.
+
+admin.site.register(Commune)
+admin.site.register(CentreVote)
+admin.site.register(BureauVote)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Commune(models.Model):
+    """
+    Représente une commune (ex: Bafoussam III)
+    """
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class CentreVote(models.Model):
+    """
+    Représente un centre de vote (souvent une école)
+    """
+
+    name = models.CharField(max_length=150)
+    commune = models.ForeignKey(Commune, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.commune.name}"
+    
+class BureauVote(models.Model):
+    """
+    Représente un bureau de vote (BV)
+    """
+
+    code = models.CharField(max_length=20, unique=True)
+    centre = models.ForeignKey(CentreVote, on_delete=models.CASCADE)
+    registered_voters = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.code} ({self.centre.name})"
