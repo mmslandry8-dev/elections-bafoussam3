e8ec64b312dab34aefa72bd24282a335f08c686c
Landry MMs <mmslandry8@gmail.com>
2026-04-05 16:32:36 +0100

    Étape 4 : Partis politiques et listes électorales

--- elections/admin.py
+++ elections/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PoliticalParty, ElectoralList
 
 # Register your models here.
+
+admin.site.register(PoliticalParty)
+admin.site.register(ElectoralList)
--- elections/models.py
+++ elections/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from core.models import Commune
 
 # Create your models here.
+
+class PoliticalParty(models.Model):
+    """
+    Représente un parti politique (RDPC, MRC, SDF...)
+    """
+
+    name = models.CharField(max_length=150)
+    acronym = models.CharField(max_length=20)
+    logo = models.ImageField(upload_to='parties/logos/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.acronym} - {self.name}"
+    
+class ElectoralList(models.Model):
+    """
+    Représente une liste candidate à une élection
+    """
+
+    name = models.CharField(max_length=150)
+    party = models.ForeignKey(PoliticalParty, on_delete=models.CASCADE)
+    commune = models.ForeignKey(Commune, on_delete=models.CASCADE)
+
+    head_candidate = models.CharField(max_length=150)  # mandataire
+
+    total_seats = models.PositiveIntegerField()  # nombre de sièges à pourvoir
+
+    def __str__(self):
+        return f"{self.name} ({self.party.acronym})"
